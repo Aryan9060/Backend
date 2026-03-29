2125ce2ad2ad5391af98611d24c9d97905e8dd62
Aryan9060 <Aryanofficial9060@gmail.com>
2026-03-29 17:07:09 +0530

    fix: correct schema definition in LoginDTO to use joi.object()

--- src/modules/auth/dto/LoginDTO.js
+++ src/modules/auth/dto/LoginDTO.js
@@ -1,8 +1,10 @@
 import joi from "joi";
 
 class LoginDTO extends BaseDTO {
-  static schima = {
+  static schima = joi.object({
     email: joi.string().email().required(),
     password: joi.string().required().min(8).max(64),
-  };
+  });
 }
+
+export default LoginDTO;
